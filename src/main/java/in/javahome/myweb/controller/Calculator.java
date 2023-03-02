4b3678eb93a0a4c7720b3520c88a9172e04f3eb6
seshadrireddy10 <seshadrireddy1492@gmail.com>
2023-03-02 17:12:07 +0530

    first commit

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,24 @@
+package in.javahome.myweb.controller;
+/*
+ * 
+ */
+public class Calculator {
+	
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		String s = null;
+		s.toUpperCase();
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int sub(int i, int j){
+		return i-j;
+	}
+}
